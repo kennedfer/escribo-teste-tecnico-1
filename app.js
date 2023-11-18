96cc6ba88f6e86361d5efa51985540344093bf5d
kennedfer <kenned14santos@gmail.com>
2023-11-18 14:24:42 -0300

    adding 'negative' verification to number

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+/**
+ * Verifica se o valor fornecido não é um número (ou é NaN).
+ * @param {any} value - O valor a ser verificado.
+ * @returns {boolean} Retorna true se o valor não for um número (ou for NaN), caso contrário, retorna false.
+ */
 function isNotNumber(value) {
     return typeof (value) != "number" || isNaN(value);
 }
 
+/**
+ * Verifica se o dividendo é (perfeitamente) divisível pelo divisor.
+ * @param {number} dividend - O dividendo.
+ * @param {number} divider - O divisor.
+ * @throws {TypeError} Lança um erro se o dividendo ou divisor não forem números.
+ * @returns {boolean} Retorna true se o dividendo for divisível pelo divisor, caso contrário, retorna false.
+ */
 function isDivisible(dividend, divider) {
     if (isNotNumber(dividend) || isNotNumber(divider)) throw new TypeError("Parametros devem ser números!");
     return dividend % divider == 0;
 }
 
+function isNegativeNumber(number) {
+    return number < 0;
+}
+
+/**
+ * Soma todos os números menores que o número fornecido que são divisíveis por 3 ou 5.
+ * @param {number} number - O número limite.
+ * @throws {TypeError} Lança um erro se o número não for um número
+ * @throws {RangeError} Lança um erro se o número for negativo.
+ * @returns {number} Retorna a soma dos números menores que o número fornecido que são divisíveis por 3 ou 5.
+ */
 function sumSmallerDivisibles(number) {
     if (isNotNumber(number)) throw new TypeError("Parametros devem ser números!");
+    if (isNegativeNumber(number)) throw new RangeError("O número deve ser positivo");
 
     let sum = 0;
 
