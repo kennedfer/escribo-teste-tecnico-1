828155b1ea176f28c7092a9f45d3f390bac7abc6
kennedfer <kenned14santos@gmail.com>
2023-11-18 14:52:22 -0300

    'errors.js', 'input.js' documenteds and finisheds

--- errors.js
+++ errors.js
@@ -1,6 +1,8 @@
+// Mensagens de erro de acordo com o tipo de erro
 const ErrorMessages = {
-    TYPE_ERROR: "Parametros devem ser números!",
+    TYPE_ERROR: "Parâmetros devem ser números!",
     RANGE_ERROR: "O número deve ser positivo!"
 }
 
-module.exports = { ErrorMessages }
+// Exporta as mensagens de erro para serem usadas em outros módulos
+module.exports = { ErrorMessages };
--- input.js
+++ input.js
@@ -1,19 +1,28 @@
+// Importa o módulo 'app' que contém as funções a serem utilizadas
 const app = require("./app");
+
+// Importa o módulo 'readline' para ler input do console
 const readLine = require("readline");
 
+// Cria uma interface de leitura para ler input do console
 const read = readLine.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-read.question("Digite um número: ", respose => {
+// Pede ao usuário para digitar um número
+read.question("Digite um número: ", response => {
     try {
-        const number = parseInt(respose)
+        // Tenta converter a resposta do usuário para um número inteiro
+        const number = parseInt(response);
+
+        // Chama a função 'sumSmallerDivisibles' do módulo 'app' e exibe o resultado
         console.log("Soma dos números menores que o seu número: " + app.sumSmallerDivisibles(number));
     } catch (error) {
+        // Captura erros, se houverem, e exibe a mensagem de erro
         console.error(error.message);
     } finally {
+        // Fecha a interface de leitura do console, independentemente do resultado
         read.close();
     }
-
-})
+});
